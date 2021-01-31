b9e924df574dc37e9a90254d142dae125f5a4ded
Wuyep Abdul <5043797-Wuyepabdul@users.noreply.gitlab.com>
2021-01-31 10:03:28 +0100

    added more logic to authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,8 +11,12 @@ export const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select("-password");
 
+      if (decoded) {
+        req.user = await User.findById(decoded.id).select("-password");
+      } else {
+        res.status(400).json({ message: "Invalid token" });
+      }
       next();
     } catch (error) {
       console.log(error.message);
